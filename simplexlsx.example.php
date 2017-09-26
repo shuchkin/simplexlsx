f96536dfd63cd1ae5579a868a49f64c7b6b6670a
Sergey Shuchkin <sergey.shuchkin@gmail.com>
2017-09-26 10:24:06 +0600

    update examples

--- simplexlsx.example.php
+++ simplexlsx.example.php
@@ -1,16 +1,18 @@
 <?php
 
-include 'simplexlsx.class.php';
+require_once __DIR__ . '/simplexlsx.class.php';
 
-$xlsx = new SimpleXLSX('countries_and_population.xlsx');
-echo '<h1>$xlsx->rows()</h1>';
-echo '<pre>';
-print_r( $xlsx->rows() );
-echo '</pre>';
+if ( $xlsx = SimpleXLSX::parse('countries_and_population.xlsx')) {
+	echo '<h1>$xlsx->rows()</h1>';
+	echo '<pre>';
+	print_r( $xlsx->rows() );
+	echo '</pre>';
 
-echo '<h1>$xlsx->rowsEx()</h1>';
-echo '<pre>';
-print_r( $xlsx->rowsEx() );
-echo '</pre>';
-
+	echo '<h1>$xlsx->rowsEx()</h1>';
+	echo '<pre>';
+	print_r( $xlsx->rowsEx() );
+	echo '</pre>';
+} else {
+	echo SimpleXLSX::parse_error();
+}
 ?>
--- simplexlsx.example3.php
+++ simplexlsx.example3.php
@@ -1,28 +1,32 @@
 <?php
+echo '<html><body><h1>XLSX to HTML</h1>';
 
 if (isset($_FILES['file'])) {
 	
-	require_once "simplexlsx.class.php";
+	require_once __DIR__ . '/simplexlsx.class.php';
 	
-	$xlsx = new SimpleXLSX( $_FILES['file']['tmp_name'] );
-	
-	echo '<h1>Parsing Result</h1>';
-	echo '<table border="1" cellpadding="3" style="border-collapse: collapse">';
-	
-	list($cols,) = $xlsx->dimension();
-	
-	foreach( $xlsx->rows() as $k => $r) {
-//		if ($k == 0) continue; // skip first row
-		echo '<tr>';
-		for( $i = 0; $i < $cols; $i++)
-			echo '<td>'.( (isset($r[$i])) ? $r[$i] : '&nbsp;' ).'</td>';
-		echo '</tr>';
-	}
-	echo '</table>';
-}
+	if ( $xlsx = SimpleXLSX::parse( $_FILES['file']['tmp_name'] ) ) {
 
-?>
-<h1>Upload</h1>
+		echo '<h2>Parsing Result</h2>';
+		echo '<table border="1" cellpadding="3" style="border-collapse: collapse">';
+
+		list( $cols, ) = $xlsx->dimension();
+
+		foreach ( $xlsx->rows() as $k => $r ) {
+			//		if ($k == 0) continue; // skip first row
+			echo '<tr>';
+			for ( $i = 0; $i < $cols; $i ++ ) {
+				echo '<td>' . ( ( isset( $r[ $i ] ) ) ? $r[ $i ] : '&nbsp;' ) . '</td>';
+			}
+			echo '</tr>';
+		}
+		echo '</table>';
+	} else {
+		echo SimpleXLSX::parse_error();
+	}
+}
+echo '<h2>Upload form</h2>
 <form method="post" enctype="multipart/form-data">
 *.XLSX <input type="file" name="file"  />&nbsp;&nbsp;<input type="submit" value="Parse" />
-</form>
+</form>';
+echo '</body></html>';
